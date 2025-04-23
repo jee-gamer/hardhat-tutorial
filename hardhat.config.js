ebcc1fb386e6d5d8c22088a8b5ebaec74aefc468
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-23 15:57:31 +0700

    Store key inside hardhat vars set (command) Don't have balance to test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 // Ensure your configuration variables are set before executing the script
 const { vars } = require("hardhat/config");
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
-const INFURA_API_KEY = vars.get(process.env.INFURA_API_KEY);
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 
 // Add your Sepolia account private key to the configuration variables
 // To export your private key from Coinbase Wallet, go to
@@ -14,7 +13,7 @@ const INFURA_API_KEY = vars.get(process.env.INFURA_API_KEY);
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = vars.get(process.env.SEPOLIA_PRIVATE_KEY);
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 module.exports = {
   solidity: "0.8.28",
